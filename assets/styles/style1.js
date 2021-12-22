e88d3e2bdfecdfbe45f085d7c1d59e839e67f30b
ccc22 <jsy23232@naver.com>
2021-12-22 09:44:21 +0900

    flat style

--- assets/styles/style1.js
+++ assets/styles/style1.js
@@ -1,70 +1,93 @@
-import { StyleSheet, Dimensions } from "react-native";
-import { getStatusBarHeight } from "react-native-status-bar-height";
+import { StyleSheet, Dimensions } from 'react-native';
+import { getStatusBarHeight } from 'react-native-status-bar-height';
 
 const styles = StyleSheet.create({
   Container: {
     flex: 1,
-    backgroundColor: "#F2F2F2",
+    backgroundColor: '#F2F2F2',
   },
   header: {
-    flexDirection: "row",
+    flexDirection: 'row',
     marginTop: getStatusBarHeight(),
     height: 150,
-    alignItems: "center",
-    justifyContent: "space-around",
+    alignItems: 'center',
+    justifyContent: 'space-around',
   },
   headerLeft: {
     paddingLeft: 20,
-    paddingRight: "20%",
-    overflow: "visible",
+    paddingRight: '20%',
+    overflow: 'visible',
     flex: 1,
   },
   headerRight: {
     width: 200,
-    position: "absolute",
-    left: "70%",
+    position: 'absolute',
+    left: '70%',
     borderRightWidth: 20,
   },
   Contents: {},
   Boxes: {
-    flexWrap: "wrap",
+    flexWrap: 'wrap',
     marginBottom: 10,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    overflow: "visible",
-    width: "100%",
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    overflow: 'visible',
+    width: '100%',
   },
   content2: { paddingHorizontal: 15, marginTop: 20, marginHorizontal: 15 },
   item: {
     height: 100,
-    width: "100%",
-    backgroundColor: "white",
+    width: '100%',
+    backgroundColor: 'white',
     marginVertical: 8,
     paddingLeft: 16,
-    overflow: "hidden",
+    overflow: 'hidden',
     borderRadius: 10,
   },
   fourbox: {
-    backgroundColor: "white",
+    backgroundColor: 'white',
     flex: 1,
     paddingVertical: 12,
-    width: "100%",
-    justifyContent: "center",
-    alignItems: "flex-end",
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'flex-end',
   },
   boxtext: {
     paddingLeft: 30,
-    width: "99%",
-    borderColor: "#F2F2F2",
-    borderStyle: "dotted",
+    width: '99%',
+    borderColor: '#F2F2F2',
+    borderStyle: 'dotted',
     borderRightWidth: 1,
   },
   footer: {
     height: 80,
-    justifyContent: "space-around",
-    alignItems: "center",
-    backgroundColor: "white",
-    flexDirection: "row",
+    justifyContent: 'space-around',
+    alignItems: 'center',
+    backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  item: {
+    flex: 5 / 6,
+    // marginHorizontal: 16,
+  },
+  num: {
+    flex: 1 / 6,
+    // marginRight: "80%",
+  },
+  title: {
+    fontSize: 32,
+  },
+  scho: {
+    fontSize: 15,
+  },
+  psysical: {
+    fontSize: 15,
+  },
+  score: {
+    fontSize: 32,
+  },
+  day: {
+    fontSize: 15,
   },
 });
 
